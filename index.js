b800e6e612b55380300a153e9a76386c87029a5b
Marco Mata <patrickstar1281@yahoo.com>
2022-07-04 23:34:33 -0500

    can now run 'node index' command to get app started

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
 const questions = () => {
@@ -19,4 +23,4 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+questions();
